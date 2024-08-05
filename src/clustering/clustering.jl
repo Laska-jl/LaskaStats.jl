6f97ab2cfe6b8884ecf3ac28ecb8ce20b575af1c
Eliassj <elias.sjolin@gmail.com>
2024-08-06 01:10:53 +0200

    d

--- src/clustering/clustering.jl
+++ src/clustering/clustering.jl
@@ -9,88 +9,123 @@ struct ComponentBuffer{D, N, T}
     z::Vector{Int64}
 end
 
-function ComponentBuffer(data::AbstractArray{T}, D::Integer, N::Integer) where {T <: Number}
+function __assert_consecutive(assignments::AbstractVector)
+    vals = unique(assignments)
+    sort!(vals)
+    vals[1] != 1 && return false
+    for i in eachindex(vals)
+        vals[i] != i && return false
+    end
+    return true
+end
+
+function __initcbuffer(
+        data::AbstractArray{T}, assignments::AbstractVector{U}, mus::AbstractVector{<:AbstractVector{T}},
+        prec::AbstractVector{<:AbstractMatrix{T}}) where {
+        T, U <: Integer}
+    __assert_consecutive(assignments) || throw(ArgumentError("Assignments not consecutive"))
+    D, N = size(data)
+    if N != length(assignments)
+        throw(ArgumentError("Assignment vector length not equal to the number of datapoints"))
+    end
+
     msize = D * D
-    z = zeros(Int64, N)
-    z[1] = size(data, 2)
+    nk = zeros(U, N)
+    for i in assignments
+        nk[i] += 1
+    end
+    nclusters = length(unique(assignments))
+    if length(mus) != length(prec) != nclusters
+        throw(ArgumentError("Number of mus or precisions not equal to the number of unique components in assignment vector"))
+    end
+    m = [MVector{D, T}(undef) for _ in 1:N]
+    p = [MMatrix{D, D, T, msize}(undef) for _ in 1:N]
+    for i in eachindex(1:nclusters)
+        m[i] = mus[i]
+        p[i] = prec[i]
+    end
     ComponentBuffer(
-        [MVector{D, T}(undef) for _ in 1:N],
-        [MMatrix{D, D, T, msize}(undef) for _ in 1:N],
-        z,
-        Base.RefValue{Int64}(0),
+        m,
+        p,
+        nk,
+        Base.RefValue{Int64}(nclusters),
         data,
-        ones(Int64, size(data, 2))
+        assignments
     )
 end
 
+# function ComponentBuffer(data::AbstractArray{T}, D::Integer, N::Integer) where {T <: Number}
+#     msize = D * D
+#     z = zeros(Int64, N)
+#     z[1] = size(data, 2)
+#     ComponentBuffer(
+#         [MVector{D, T}(undef) for _ in 1:N],
+#         [MMatrix{D, D, T, msize}(undef) for _ in 1:N],
+#         z,
+#         Base.RefValue{Int64}(0),
+#         data,
+#         ones(Int64, size(data, 2))
+#     )
+# end
+
+# BUG: Many functions calling __data looking for all datapoints of component
+# are only getting data point n = c
 function __data(b::ComponentBuffer)
     b.data
 end
 
-function __data(b::ComponentBuffer, i::Union{Integer, AbstractUnitRange})
+function __data(b::ComponentBuffer, i)
     @view b.data[:, i]
 end
 
 function __logprobs_t_dist!(
         logv::AbstractVector{T}, b::ComponentBuffer{D, N, T}, data) where {D, N, T}
-    n_clusters = __nactive(b)
     nk = __nk(b)
     cmeans = __means(b)
     d = length(cmeans[1])
     cprec = __precisions(b)
 
-    @show nk
-    m = @. inv((nk / (d + 1)) * cprec)
+    m = @. inv(cprec)
 
-    n = 0
-    @show m
     for i in 1:__nactive(b)
-        n += 1
-        @show n
         logv[i] = log(nk[i]) +
                   logpdf(
-            MvTDist(
-                1,
+            MvNormal(
                 Vector{T}(cmeans[i]),
                 PDMats.PDMat(m[i], cholesky(Hermitian(m[i])))
             ), data)
     end
 end
 
+function __logprob_t_dist_new(b::ComponentBuffer, mu, prec, alpha, data)
+    m = prec
+    log(alpha) + logpdf(MvNormal(
+            mu,
+            PDMats.PDMat(m, cholesky(Hermitian(m)))
+        ), data)
+end
+
 function __update_component_params!(b::ComponentBuffer, component)
-    m = __nk(b, component)
+    k0 = __nk(b, component)
     datainds = findall(x -> x == component, __assignments(b))
-    data = __data(b)[:, datainds]
-    # BUG: Sometimes, data is empty??
+    data = __data(b, datainds)
     data_mean = vec(mean(data, dims = 2))
     mu0 = __means(b, component)[]
     prec0 = __precisions(b, component)[]
-    @show data data_mean mu0 prec0
     __setparams!(b, component,
-        ((m + 1) * mu0 + data_mean * m) / (m * 2 + 1),
+        (k0 * mu0 + data_mean) / (k0 + 1),
         inv(
             prec0 + pairwisedeviationpsum(data, data_mean) +
-            ((m^2 + 1) / (m * 2 + 1)) * (data_mean - mu0) *
+            (k0 / (k0 + 1)) * (data_mean - mu0) *
             transpose(data_mean - mu0)
         )
     )
-    @show __precisions(b, component)
 end
-
-# NOTE: Do we need to check n_active etc here?
-# Unnecessary since the state literally is from the last iteration
-
-# function __assignfromprior!(
-#         b::ComponentBuffer, means::AbstractVector, precisions::AbstractVector)
-#     n_components = length(means)
-#     b.means[begin:n_components] = means
-#     b.prec[begin:n_components] = precisions
-# end
 
 # Remove component 'n' and, if the component becomes empty, remove it, lower number of active
 # components and return 0. Otherwise, return the index of the component that has been changed
 function __removedata!(b::ComponentBuffer, n::Integer)
-    c = __assignment(b, n)
+    c = __assignments(b, n)
     b.z[n] = 0
     __lowernk!(b, c)
     # If this was the only member of the component, remove the component
@@ -116,7 +151,7 @@ end
 
 __assignments(b::ComponentBuffer) = b.z
 # get assignment of data point n
-__assignment(b::ComponentBuffer, n::Integer) = b.z[n]
+__assignments(b::ComponentBuffer, n) = b.z[n]
 # Assign datapoint 'n' to luster 'c'
 function __assign!(b::ComponentBuffer, n::Integer, c::Integer)
     if c > __nactive(b)
@@ -141,6 +176,15 @@ function __assign!(b::ComponentBuffer, n::Vector{Int64}, c::Integer)
     end
     b.z[n] .= c
     __upnk!(b, c, length(n))
+end
+
+function __assign_new!(b::ComponentBuffer, n::Integer, c::Integer)
+    if c != __nactive(b) + 1
+        throw(ArgumentError("Attempted to assign data to new non-consecutive component $c of ComponentBuffer with $(__nactive(b)) active components"))
+    end
+    b.z[n] = c
+    __bumpnactive!(b)
+    __setnk!(b, c, 1)
 end
 
 # Unassign datapoint
@@ -180,22 +224,16 @@ end
 # Add parameters for component
 function __setparams!(b::ComponentBuffer{D, N, T}, component::Integer,
         mean::AbstractVector{T}, prec::AbstractArray{T}) where {D, N, T}
-    if component > __nactive(b) + 1
-        throw(ArgumentError("Attempted to assign new component at non-consecutive index from the last active component at $(__nactive(b))"))
+    if component > __nactive(b)
+        throw(ArgumentError("Attempted to assign non-existent component at index $component"))
     end
     b.means[component] = mean
     b.prec[component] = prec
-    component > __nactive(b) && __bumpnactive!(b)
 end
 
 function __setparams!(b::ComponentBuffer{D, N, T}, component::Integer,
         params::Tuple{<:AbstractVector{T}, <:AbstractMatrix{T}}) where {D, N, T}
-    if component > __nactive(b) + 1
-        throw(ArgumentError("Attempted to assign new component at non-consecutive index from the last active component at $(__nactive(b))"))
-    end
-    b.means[component] = params[1]
-    b.prec[component] = params[2]
-    component > __nactive(b) && __bumpnactive!(b)
+    __setparams!(b, component, params[1], params[2])
 end
 
 function __getparams(b::ComponentBuffer, c::Integer)
@@ -240,8 +278,6 @@ function cluster3(data, z_init, alpha, maxiter = 1000)
     z = deepcopy(z_init)
     res_assignments[:, 1] = z
 
-    cbuffer = ComponentBuffer(data, D, N)
-
     cvar_data = cov(data, dims = 2)
     prec_data = inv(cvar_data)
     mean_data = mean(data, dims = 2)
@@ -271,7 +307,8 @@ function cluster3(data, z_init, alpha, maxiter = 1000)
 
     # Set initial params from prior above
 
-    __setparams!(cbuffer, 1, component_means[1][1], component_precisions[1][1])
+    cbuffer = __initcbuffer(
+        data, z_init, [component_means[1][1]], [component_precisions[1][1]])
 
     # Preallocate vector for log probs of each cluster. Will be the length of data since
     # having more clusters than datapoints would be useless
@@ -289,36 +326,43 @@ function cluster3(data, z_init, alpha, maxiter = 1000)
             changed_component = __removedata!(cbuffer, i)
 
             cluster_means = clustermeans(
-                data[:, Not(i)], z[Not(i)], __nactive(cbuffer), __nk(cbuffer))
+                data[:, Not(i)], __assignments(cbuffer, Not(i)), __nactive(cbuffer), __nk(cbuffer))
             n_clusters = __nactive(cbuffer)
 
             # update non-cluster specific parameters
             ## r
+
             r_deg, r_scale = conditional_mvn_prec(
                 n_clusters, D, cvar_data,
                 cluster_means, vec(lambda[:, iteration - 1]))
             r[:, :, iteration] = rand(Wishart(
                 r_deg, Matrix{Float64}(Symmetric(inv(r_scale)))))
+
             ## lambda
-            lambda_mean, lambda_sig = conditional_mvn_mu(
-                prec_data, r[:, :, iteration], mean_data, n_clusters, mean_data)
+
+            cmeans = mean(__means(cbuffer))
+            lambda_mean, lambda_sig = conditional_lambda(
+                r[:, :, iteration - 1], prec_data, vec(mean_data), __nactive(cbuffer), cmeans
+            )
             lambda[:, iteration] = rand(MvNormal(vec(lambda_mean), lambda_sig))
 
             # Update parameters of the changed cluster
-# NOTE: Check data of changed component here!
+            # NOTE: Check data of changed component here!
+
             if !iszero(changed_component)
                 __update_component_params!(cbuffer, changed_component)
             end
+
             # Calculate probabilities for each cluster
             __logprobs_t_dist!(logp_clusters, cbuffer, __data(cbuffer, i))
 
             # Probability of new cluster
-            mu_new = rand(MvNormal(lambda[:, iteration], Symmetric(r[:, :, iteration])))
+            mu_new = rand(MvNormal(
+                lambda[:, iteration], Symmetric(inv(r[:, :, iteration]))))
             # TODO: Figure out updating precision priors!
-            prec_new = rand(Wishart(D, Matrix{Float64}(Symmetric(inv(w[:, :, 1])))))
+            prec_new = rand(Wishart(D, cvar_data))
             logp_clusters[n_clusters + 1] = log(alpha) + logpdf(
-                MvNormal(mu_new, Symmetric(inv(prec_new))), __data(cbuffer, i))
-
+                MvNormal(mu_new, Hermitian(inv(prec_new))), __data(cbuffer, i))
             max_logp_clusters = maximum(@view(logp_clusters[begin:(__nactive(cbuffer) + 1)]))
             logp_clusters[begin:(__nactive(cbuffer) + 1)] .-= max_logp_clusters
 
@@ -328,8 +372,11 @@ function cluster3(data, z_init, alpha, maxiter = 1000)
             z_new = rand(Categorical(loc_probs))
 
             # Add new params if new cluster
-            if z_new == n_clusters + 1
+            if z_new == __nactive(cbuffer) + 1
+                println("=========NEW")
+                __assign_new!(cbuffer, i, z_new)
                 __setparams!(cbuffer, n_clusters + 1, mu_new, prec_new)
+                __update_component_params!(cbuffer, z_new)
             end
             # Assign datapoint to selected cluster
             __assign!(cbuffer, i, z_new)
@@ -347,7 +394,7 @@ end
 
 function cluster(data, alpha, mu0, sigma0, sigma_y, c_init, maxiter = 1000)
     # Dimensionality and number of data points
-    D, N = size(data)
+    N = size(data, 2)
 
     # Result matrix
     res_assignments = Matrix{Int64}(undef, N, maxiter)
@@ -434,131 +481,6 @@ function cluster(data, alpha, mu0, sigma0, sigma_y, c_init, maxiter = 1000)
     return res_assignments
 end
 
-function cluster2(data, z_init, alpha, maxiter = 1000)
-    D, N = size(data)
-    res_assignments = Matrix{Int64}(undef, N, maxiter)
-    z = deepcopy(z_init)
-    res_assignments[:, 1] = z
-
-    cvar_data = cov(data, dims = 2)
-    prec_data = inv(cvar_data)
-    mean_data = mean(data, dims = 2)
-
-    # Initialize parameter vectors/arrays
-    r = Array{Float64}(undef, D, D, maxiter) # Precision hyperparameter of means
-    lambda = Matrix{Float64}(undef, D, maxiter) # Mean hyperparameter for means
-    component_means = Vector{Matrix{Float64}}(undef, maxiter)
-
-    w = Array{Float64}(undef, D, D, maxiter) # Scale matrix for component precision prior
-    component_precisions = Vector{Array{Float64}}(undef, maxiter)
-
-    # Sample initial values for component means
-    r[:, :, 1] = rand(Wishart(D, cvar_data^(-2)))
-    lambda[:, 1] = rand(MvNormal(vec(mean(data, dims = 2)), cvar_data))
-    component_means[1] = Matrix{Float64}(undef, D, maximum(z_init))
-    @views for c in 1:maximum(z_init)
-        component_means[1][:, c] = rand(MvNormal(lambda[:, 1], Symmetric(inv(r[:, :, 1]))))
-    end
-
-    # Sample initial values for component precisions
-    w[:, :, 1] = rand(Wishart(D, cvar_data))
-    component_precisions[1] = Array{Float64}(undef, D, D, maximum(z_init))
-    @views for c in 1:maximum(z_init)
-        component_precisions[1][:, :, c] = rand(Wishart(
-            D, Matrix{Float64}(Symmetric(inv(w[:, :, 1])))))
-    end
-
-    nk = [count(z .== c) for c in 1:maximum(z)]
-    n_clusters = length(nk)
-
-    # Preallocate vector for log probs of each cluster. Will be the length of data since
-    # having more clusters than datapoints would be useless
-    logp_clusters = Vector{Float64}(undef, N)
-
-    # Main iteration loop
-    for iteration in 2:maxiter
-        iszero(iteration % 100) && println(iteration)
-
-        # Use posteriors from last iteration as priors
-        component_means[iteration] = hcat(component_means[iteration - 1], zeros(D, 1))
-        component_precisions[iteration] = cat(
-            component_precisions[iteration - 1], zeros(D, D), dims = 3)
-
-        for i in Base.OneTo(N)
-            # Remove datapoint from its cluster
-            c = z[i]
-            nk[c] -= 1
-
-            # If cluster is empty, remove it and move the data in the last clusters to it
-            if iszero(nk[c])
-                last_inds = findall(x -> x == n_clusters, z)
-                nk[c] = nk[n_clusters]
-                pop!(nk)
-                z[last_inds] .= c
-                n_clusters -= 1
-            end
-
-            cluster_means = clustermeans(data[:, Not(i)], z[Not(i)], n_clusters, nk)
-
-            # update non-cluster specific parameters
-            ## r
-            r_deg, r_scale = conditional_mvn_prec(
-                n_clusters, D, cvar_data,
-                cluster_means, vec(lambda[:, iteration - 1]))
-            r[:, :, iteration] = rand(Wishart(
-                r_deg, Matrix{Float64}(Symmetric(inv(r_scale)))))
-            ## lambda
-            lambda_mean, lambda_sig = conditional_mvn_mu(
-                prec_data, r[:, :, iteration], mean_data, n_clusters, mean_data)
-            lambda[:, iteration] = rand(MvNormal(vec(lambda_mean), lambda_sig))
-
-            for c in Base.OneTo(n_clusters)
-                data_inds = findall(x -> x == c, z)
-
-                component_means[iteration][:, c], component_precisions[iteration][:, :, c] = conditional_component_params(
-                    nk[c], component_means[iteration][:, c], data[:, data_inds],
-                    cluster_means[:, c], component_precisions[iteration][:, :, c])
-
-                logp_clusters[c] = log(nk[c]) + logpdf(
-                    conditional_t_dist(D, D, component_means[iteration][:, c], nk[c],
-                        component_precisions[iteration][:, :, c]), data[:, i])
-            end # Closes cluster iteration loop
-
-            # Probability of new cluster
-            mu_new = rand(MvNormal(lambda[:, iteration], Symmetric(r[:, :, iteration])))
-            component_means[iteration][:, n_clusters + 1] = mu_new
-            # TODO: Figure out updating precision priors!
-            prec_new = rand(Wishart(D, Matrix{Float64}(Symmetric(inv(w[:, :, 1])))))
-            component_precisions[iteration][:, :, n_clusters + 1] = prec_new
-            logp_clusters[n_clusters + 1] = log(alpha) + logpdf(
-                MvNormal(mu_new, Symmetric(inv(prec_new))), data[:, i])
-            logp_clusters[begin:(n_clusters + 1)]
-
-            max_logp_clusters = maximum(@view(logp_clusters[begin:(n_clusters + 1)]))
-            @. logp_clusters[begin:(n_clusters + 1)] -= max_logp_clusters
-
-            loc_probs = exp.(@view(logp_clusters[begin:(n_clusters + 1)]))
-            loc_probs ./= sum(loc_probs[begin:(n_clusters + 1)])
-
-            z_new = rand(Categorical(loc_probs))
-
-            # TODO: Add handling of new cluster parameters
-            if z_new == n_clusters + 1
-                push!(nk, 0)
-                n_clusters += 1
-            end
-            z[i] = z_new
-            nk[z_new] += 1
-        end # Closes data point iteration loop
-        res_assignments[:, iteration] = z
-    end # Closes main iteration loop
-    return (
-        assignments = res_assignments,
-        means = component_means,
-        prec = component_precisions
-    )
-end
-
 function conditional_t_dist(n_m, d, mu_m, k_m, prec_m)
     MvTDist(n_m - d + 1, mu_m,
         Matrix{Float64}(Symmetric(inv((k_m * (n_m - d + 1) / (k_m + 1)) * prec_m))))
@@ -634,12 +556,29 @@ function conditional_precision(data_j, nj, β, mu_j, cluster_means)
     return deg, scale
 end
 
+function alphaprob(α, k, n)
+    (α^(k - 3 / 2) * exp(-1.0 / (2 * α)) * gamma(α)) /
+    gamma(n + α)
+end
+
 # Multivariate normal with known precision (r)
 function conditional_lambda(r::AbstractMatrix, s_y::AbstractMatrix, mu_y::AbstractVector,
         n_j::Integer, samplemean::AbstractVector)
     mu = inv(s_y + n_j * r) * (s_y * mu_y + n_j * r * samplemean)
     s = s_y + n_j * r
     return mu, s
+end
+
+function __conditional_lambda(b::ComponentBuffer, r0, lambda0)
+    k0 = __nactive(b)
+    data = __data(b)
+    data_mean = vec(mean(data, dims = 2))
+    cmeans = __means(b)
+    xbar = sum(cmeans) ./ k0
+    mu = (k0 * lambda0 + xbar) / (k0 + 1)
+    prec = inv(r0 + pairwisedeviationpsum(cmeans, xbar) +
+               (k0 / (k0 + 1)) * ((xbar - lambda0) * transpose(xbar - lambda0)))
+    return mu, prec
 end
 
 function conditional_r(s_y, k, λ, cluster_means)
@@ -668,7 +607,7 @@ function beta_posterior(precisions_c, w, k)
 end
 
 function calc_contour(dist, xrange, yrange)
-    [logpdf(dist, [x, y]) for x in xrange, y in yrange]
+    [pdf(dist, [x, y]) for x in xrange, y in yrange]
 end
 
 function MAP_assignments(immresult::AbstractMatrix{<:Integer}, burnin, skip)
@@ -681,9 +620,23 @@ function MAP_assignments(immresult::AbstractMatrix{<:Integer}, burnin, skip)
 end
 
 function pairwisedeviationpsum(
-        m::AbstractMatrix{<:AbstractFloat}, μ::Vector{<:AbstractFloat})
-    sum = m .- μ
-    sum * transpose(sum)
+        m::AbstractArray{<:AbstractFloat}, μ::Vector{<:AbstractFloat})
+    sum = zeros(length(μ), length(μ))
+    for i in axes(m, 2)
+        diff = (m[:, i] - μ)
+        sum += diff * transpose(diff)
+    end
+    return sum
+end
+
+function pairwisedeviationpsum(
+        m::AbstractVector{<:AbstractVector}, μ::AbstractVector{<:AbstractFloat})
+    sum = zeros(length(μ), length(μ))
+    for i in eachindex(m)
+        diff = m[i] - μ
+        sum += diff * transpose(diff)
+    end
+    return sum
 end
 
 function adjgraph(immresult::AbstractMatrix{<:Integer})
