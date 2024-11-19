ca74a1d5799b06e2dfa0179629084a034c0219d0
Eliassj <elias.sjolin@gmail.com>
2024-11-19 18:42:40 +0100

    Missedspikes now return nothing is no 0-crossing is detected

--- src/quality/missed.jl
+++ src/quality/missed.jl
@@ -6,15 +6,18 @@ Calculate the percentage of missing spikes based on the distribution of their am
 Attempts to fit a gaussian to the distribution of amplitude of amplitudess and calculate the number of 
 spikes that fell below the detection limit.
 """
-function missedspikes(amplitudes::AbstractVector; kernelwidth=3, histbins::Integer=500)
-    h = fit(StatsBase.Histogram, amplitudes, nbins=histbins)
+function missedspikes(amplitudes::AbstractVector; kernelwidth = 3, histbins::Integer = 500)
+    h = fit(StatsBase.Histogram, amplitudes, nbins = histbins)
     gau = imfilter(h.weights, Kernel.gaussian((kernelwidth,)))
 
     maxind = argmax(gau)
 
     @views begin
-        diffind = findfirst(Base.Fix2(isless, zero(eltype(gau))), @. gau[maxind:end] - gau[begin]) + maxind
+        diffind = findfirst(
+            Base.Fix2(isless, zero(eltype(gau))), @. gau[maxind:end] - gau[begin]) + maxind
     end
+
+    isnothing(diffind) && return 0.0
 
     nmissed = sum(@view(gau[diffind:end]))
 
@@ -28,12 +31,13 @@ end
 Estimate the percentage of missed spikes `spikes_per_partition` spikes at a time.
 Estimation is carried out in the same way as [`LaskaCore.missedspikes`](@ref).
 """
-function missedspikespartition(amplitudes::AbstractVector, spikes_per_partition::Integer; kernelwidth = 3, histbins::Integer = 500)
+function missedspikespartition(amplitudes::AbstractVector, spikes_per_partition::Integer;
+        kernelwidth = 3, histbins::Integer = 500)
     n_partitions = ceil(length(amplitudes) / spikes_per_partition) |> Int64
     out = Vector{Float64}(undef, n_partitions)
 
     for (i, amps) in enumerate(Iterators.partition(amplitudes, spikes_per_partition))
-        out[i] = missedspikes(amps; kernelwidth = kernelwidth, histbins=histbins)
+        out[i] = missedspikes(amps; kernelwidth = kernelwidth, histbins = histbins)
     end
 
     return out
