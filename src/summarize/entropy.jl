acd41f55234f7a3eb36e0f3e7448585d10a82397
Eliassj <elias.sjolin@gmail.com>
2024-11-26 19:27:48 +0100

    remove @show

--- src/summarize/entropy.jl
+++ src/summarize/entropy.jl
@@ -5,17 +5,13 @@ end
 
 function __isientropy(logisi)
     minisi, maxisi = extrema(logisi)
-    @show minisi maxisi
     binsize = 0.02 * (maxisi - minisi)
-    @show binsize
     h = fit(StatsBase.Histogram, logisi, minisi:binsize:maxisi)
     filtered = imfilter(h.weights, Kernel.gaussian((3,)))
     out = 0.0
     filtered ./= sum(filtered)
-    @show sum(filtered)
     for i in eachindex(filtered)
         out += filtered[i] * log2(filtered[i])
     end
-
     return -out
 end
